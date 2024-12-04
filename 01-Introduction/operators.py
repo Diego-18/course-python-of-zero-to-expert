eab6a82bf8a31a9c43e1d871b3e6b6682d48fd9a
Diego Jose Chavez Chirinos <ingdiegochavez18@gmail.com>
2024-12-03 22:44:20 -0500

    feat: :sparkles: operators

---
+++ 01-Introduction/operators.py
@@ -0,0 +1,26 @@
+#Numbers Operators
+a = 10
+b = 10
+print("S:", a + b)
+print("R:", a - b)
+print("M:", a * b)
+print("D:", a / b)
+print("Potenciación:", a ** b)
+print("Parte entera de la división:", a // b)
+print("Módulo:", a % b)
+a /= 2
+print(a)
+operation_1  = 2 + 3 * 4
+operation_2  = (2 + 3) * 4
+print(operation_1)
+print(operation_2)
+operation_3 = (2+3) * (4**2)/ 8 - 1
+print(operation_3)
+
+# Comparations Operators
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
